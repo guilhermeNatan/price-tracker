84ed2180b5d32cedad780071b4f7879695b685f5
guilhermeNatan <guilherme.natan.barbosa@gmail.com>
2021-01-02 16:37:32 -0300

    create a DataLoader

--- src/main/java/com/price/tracker/config/DataLoader.java
+++ src/main/java/com/price/tracker/config/DataLoader.java
@@ -1,5 +1,10 @@
 package com.price.tracker.config;
 
+import com.price.tracker.entity.PlatformEnum;
+import com.price.tracker.entity.StoreEnum;
+import com.price.tracker.factory.PlatformFactory;
+import com.price.tracker.factory.StoreFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
@@ -7,9 +12,16 @@ import org.springframework.stereotype.Component;
 @Component
 public class DataLoader implements ApplicationRunner {
 
+    @Autowired
+    private PlatformFactory platformFactory;
+    @Autowired
+    private StoreFactory storeFactory;
+
     @Override
     public void run(ApplicationArguments args) throws Exception {
-
-
+        final Boolean SAVE = true;
+        platformFactory.create(SAVE, PlatformEnum.PLAYSTATION_4);
+        platformFactory.create(SAVE, PlatformEnum.PLAYSTATION_5);
+        storeFactory.create(SAVE, StoreEnum.PLAYSTATION_STORE, "https://store.playstation.com");
     }
 }
