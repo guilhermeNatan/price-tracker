d57c61103536991265aacd700aecd1cc82f5c959
guilhermeNatan <guilherme.natan.barbosa@gmail.com>
2021-01-17 20:08:37 -0300

    Disable tasklet

--- src/main/java/com/price/tracker/tasklets/ScrabTasklet.java
+++ src/main/java/com/price/tracker/tasklets/ScrabTasklet.java
@@ -10,8 +10,10 @@ public class ScrabTasklet {
     @Autowired
     private Scrab scrab;
 
-    @Scheduled(cron = "0 0 0/6 1/1 * ?")
+//    @Scheduled(cron = "0 0/1 * 1/1 * ?")
+//    @Scheduled(cron = "0 0 0/6 1/1 * ?")
     public void scrabPsStore() {
+
         scrab.psStoreScrab("https://store.playstation.com/pt-br/category/85448d87-aa7b-4318-9997-7d25f4d275a4");
     }
 }
