e91dc2a2daf6f517e31fb00fb92e387cb12b9c9a
guilhermeNatan <guilherme.natan.barbosa@gmail.com>
2021-01-23 15:50:00 -0300

    Auth flux

--- server/src/main/java/com/price/tracker/config/DataLoader.java
+++ server/src/main/java/com/price/tracker/config/DataLoader.java
@@ -1,8 +1,10 @@
 package com.price.tracker.config;
 
 import com.price.tracker.entity.PlatformEnum;
+import com.price.tracker.entity.RoleName;
 import com.price.tracker.entity.StoreEnum;
 import com.price.tracker.factory.PlatformFactory;
+import com.price.tracker.factory.RoleFactory;
 import com.price.tracker.factory.StoreFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
@@ -17,11 +19,15 @@ public class DataLoader implements ApplicationRunner {
     @Autowired
     private StoreFactory storeFactory;
 
+    @Autowired
+    private RoleFactory roleFactory;
+
     @Override
     public void run(ApplicationArguments args) throws Exception {
         final Boolean SAVE = true;
         platformFactory.create(SAVE, PlatformEnum.PLAYSTATION_4);
         platformFactory.create(SAVE, PlatformEnum.PLAYSTATION_5);
         storeFactory.create(SAVE, StoreEnum.PLAYSTATION_STORE, "https://store.playstation.com");
+        roleFactory.createIfNotExist(SAVE, RoleName.ROLE_CLIENTE);
     }
 }
