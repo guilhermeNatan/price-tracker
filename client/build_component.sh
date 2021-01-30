344d3cbe35276cecb152f1f49a8b4e9ab71554ad
guilhermeNatan <guilherme.natan.barbosa@gmail.com>
2021-01-30 17:35:05 -0300

    Fix build_component.sh script

--- client/build_component.sh
+++ client/build_component.sh
@@ -10,11 +10,11 @@ read screenname
 echo -n "Entre com o nome do componente <ex Header>: "
 read name
 
-mkdir  src/pages/$screenname/components/$name
+mkdir  src/pages/"$screenname"/components/$name
 sed -e "s/TemplateScreen/${name}/g" -e 's/Component/PureComponent/g' src/TemplateModule/TemplateScreen.js >  src/pages/$screenname/components/$name/$name.js
 sed "s/TemplateScreen/${name}/g" src/TemplateModule/index.js >  src/pages/$screenname/components/$name/index.js
 
-cp src/TemplateModule/style.js   src/pages/$screenname/components/$name
+cp src/TemplateModule/style.js   src/pages/$screenname/components/$name/"${name}Styles.js"
 
 
 
