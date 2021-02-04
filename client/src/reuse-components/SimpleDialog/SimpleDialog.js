29d05dac1af4c46e622e5a59c1592ba67fb4605a
guilhermeNatan <guilherme.natan.barbosa@gmail.com>
2021-02-03 21:36:29 -0300

    Create dialog component

---
+++ client/src/reuse-components/SimpleDialog/SimpleDialog.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+
+
+export default function SimpleDialog({children, buttonName, title}) {
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+      <div>
+        <Button  color="secondary"
+                 onClick={handleClickOpen}
+        >
+          {buttonName}
+        </Button>
+        <Dialog
+            open={open}
+            onClose={handleClose}
+            aria-labelledby="alert-dialog-title"
+            aria-describedby="alert-dialog-description"
+        >
+          <DialogTitle id="alert-dialog-title">{title}</DialogTitle>
+          <DialogContent>
+            <DialogContentText id="alert-dialog-description">
+              {children}
+            </DialogContentText>
+          </DialogContent>
+        </Dialog>
+      </div>
+  );
+}
