c98e85b01df1db097a26b204bf66248eec349ac2
guilhermeNatan <guilherme.natan.barbosa@gmail.com>
2021-02-28 09:32:07 -0300

    Add a profiler menu

--- client/src/layout/drawable-layout/components/UserMenu/UserMenu.js
+++ client/src/layout/drawable-layout/components/UserMenu/UserMenu.js
@@ -1,11 +1,43 @@
 import React, {Component} from 'react';
 import styles from './UserMenuStyles';
 import PersonOutlineTwoToneIcon from '@material-ui/icons/PersonOutlineTwoTone';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
 
 class UserMenu extends Component {
+  constructor(props, context) {
+    super(props, context);
+    this.state = {
+      anchorEl: null
+    }
+  }
+  setAnchorEl = (anchorEl) => this.setState({anchorEl});
+
+  handleClick = (event) => {
+    this.setAnchorEl(event.currentTarget);
+  };
+
+  handleClose = () => {
+    this.setAnchorEl(null);
+  };
+
   render() {
+    const {anchorEl} = this.state;
     return (
-       <PersonOutlineTwoToneIcon />
+        <div>
+          <PersonOutlineTwoToneIcon  aria-controls="simple-menu" aria-haspopup="true" onClick={this.handleClick} />
+          <Menu
+              id="simple-menu"
+              anchorEl={anchorEl}
+              keepMounted
+              open={Boolean(anchorEl)}
+              onClose={this.handleClose}
+          >
+            <MenuItem onClick={this.handleClose}>Profile</MenuItem>
+            <MenuItem onClick={this.handleClose}>My account</MenuItem>
+            <MenuItem onClick={this.handleClose}>Logout</MenuItem>
+          </Menu>
+        </div>
     );
   }
 }
